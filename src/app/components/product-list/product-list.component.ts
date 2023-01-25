1628c734cd62ab09d8d9a43b0420b0744affb799
oumao <evansotis2015@gmail.com>
2023-01-25 23:45:52 +0300

    Fetched data to ProductList

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service'
+
+import { Product } from 'src/app/models/Product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
 
+  products: Product[] = []
+
+  constructor(private httpService: HttpService){}
+
+  ngOnInit(){
+    this.httpService.getProducts().subscribe(res => {
+      this.products = res;
+    })
+  }
 }
