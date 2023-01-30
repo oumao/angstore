5952b775bb59350b27473f2880b395b0da45a22c
oumao <evansotis2015@gmail.com>
2023-01-30 22:20:01 +0300

    Pass data to child component for Items

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/models/Product';
+import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service'
 
 @Component({
   selector: 'app-product-item',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent {
+  @Input() product: Product
+  pickedItem = '1'
+  productCount: string[] | undefined = ['1', '2', '3', '4', '5']
 
+  constructor(private productService: ProductService, 
+              private cartService: CartService) {
+    this.product = { id:0, name:'', price:0, url:'', description:''}
+  }
+
+  changeSelected(value: unknown): void {
+    this.pickedItem = value as unknown as string
+  }
+
+  addItemToCart(item: Product): void{
+    const itemsInCart = this.cartService.getCartItems();
+    let itemInCart = itemsInCart.find((elem) => elem.id === item.id)
+
+    // Check to see if the item is already added
+    if(itemInCart){
+      itemInCart.quantity = this.pickedItem;
+      itemInCart ? this.productService.addProduct(itemsInCart) : null
+    }
+  }
 }
