24cf5827dfbc6c5a816e5324246f76031e8be32b
oumao <evansotis2015@gmail.com>
2023-01-30 22:18:39 +0300

    Setup routes for Cart and Products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component'
+import { ProductItemComponent } from './components/product-item/product-item.component'
+import { ProductListComponent } from './components/product-list/product-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: ProductListComponent},
+  {path: "cart", component: CartComponent},
+  {path: "product", component: ProductItemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
