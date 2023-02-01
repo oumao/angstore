ce4231b2739ba1d75ab2603a755381391817e160
oumao <evansotis2015@gmail.com>
2023-02-01 23:17:43 +0300

    Added addToCart, clearCart and getCart

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,18 +3,35 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'
 
 import { Product } from '../models/Product';
+import { CartProduct } from '../models/CartProduct';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
-  products: Product[] = []
+  myStore = window.localStorage;
 
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]>{
     return this.http.get<Product[]>('../../assets/data.json')
   }
+
+  // Adding an Item to a cart
+  addToCart(product: CartProduct[]): void {
+    this.myStore.setItem('cart', JSON.stringify(product));
+  }
+
+  // Clear Cart 
+  clearCart(): void {
+    this.myStore.clear();
+  }
+
+  // Getting Items in a Cart
+  getCart(): CartProduct[] {
+    const getItem = this.myStore.getItem('cart');
+    return getItem? JSON.parse(getItem): [];
+  }
+
 }
