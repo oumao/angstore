461ad7c7fb29f462f134b2ea6e1dae56b4dc8769
oumao <evansotis2015@gmail.com>
2023-02-05 23:43:20 +0300

    Added Checkout Success Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component'
+import { CheckoutComponent } from './components/checkout/checkout.component'
 import { ProductItemComponent } from './components/product-item/product-item.component'
 import { ProductListComponent } from './components/product-list/product-list.component'
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path: "", redirectTo: 'home',pathMatch: 'full'},
   {path: "home", component: ProductListComponent},
   {path: "cart", component: CartComponent},
-  {path: "product", component: ProductItemComponent}
+  {path: "product", component: ProductItemComponent},
+  {path: "checkout", component: CheckoutComponent}
 ];
 
 @NgModule({
