90c4524464c638a2149178b4c746437b9b8fecd1
oumao <evansotis2015@gmail.com>
2023-02-05 23:12:05 +0300

    Worked on Display Items, Checkout form and Total Price

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CartProduct } from 'src/app/models/CartProduct'
+import { productcount } from 'src/app/models/Product'
+import { HttpService } from 'src/app/services/http.service'
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  cartItems: CartProduct[] = [];
+  productCount: string[] = productcount
+  cartTotal: number = 0;
+  fullName: string;
+  address: string;
+  creditCard: number | string
 
+  constructor(private cartService: HttpService){
+    this.fullName = '';
+    this.address = '';
+    this.creditCard = '';
+  }
+
+  ngOnInit(): void {
+    this.cartItems = this.cartService.getCart();
+    this.calculateProductTotal();
+  }
+
+  calculateProductTotal(): void {
+    this.cartTotal = this.cartItems.reduce((curr: number, value: any) => {
+      return curr + value.price * value.quantity as unknown as number;
+    }, 0);
+
+    this.cartTotal = Number(this.cartTotal.toFixed(2));
+  }
+
+  onCheckOut(): void{
+
+  }
 }
