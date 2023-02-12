8d64b1ca8737b329c732d90c3cc38aee5f59e03a
oumao <evansotis2015@gmail.com>
2023-02-12 15:01:10 +0300

    Added Detail Item Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component'
 import { CheckoutComponent } from './components/checkout/checkout.component'
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component'
 import { ProductItemComponent } from './components/product-item/product-item.component'
 import { ProductListComponent } from './components/product-list/product-list.component'
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path: "home", component: ProductListComponent},
   {path: "cart", component: CartComponent},
   {path: "product", component: ProductItemComponent},
+  {path: "item-detail/:id", component: ProductItemDetailComponent},
   {path: "checkout", component: CheckoutComponent}
 ];
 
