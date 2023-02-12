95f0516432c9248d51ff912ead12f718d6138005
oumao <evansotis2015@gmail.com>
2023-02-12 22:44:36 +0300

    Added CartItem interface

--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -6,4 +6,14 @@ export interface Product{
     description: string;
 }
 
+export interface Item {
+    id: number;
+    name: string;
+    price: number;
+    url: string;
+    description: string;
+    quantity: string;
+} 
+
+
 export const productcount: string[] = ['1', '2', '3', '4', '5']
