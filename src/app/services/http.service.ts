7c1eca8fc55bc854c6714a4498f3edc9035e2fc2
oumao <evansotis2015@gmail.com>
2023-02-12 22:44:02 +0300

    Refactor the httpService

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,14 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'
 
 import { Product } from '../models/Product';
-import { CartProduct } from '../models/CartProduct';
+import { Item } from '../models/Product';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  myStore = window.localStorage;
 
   constructor(private http: HttpClient) { }
 
@@ -18,20 +17,43 @@ export class HttpService {
     return this.http.get<Product[]>('assets/data.json')
   }
 
-  // Adding an Item to a cart
-  addToCart(product: CartProduct[]): void {
-    this.myStore.setItem('cart', JSON.stringify(product));
-  }
+  // // Adding an Item to a cart
+  // addToCart(product: Item[]): void {
+  //   window.localStorage.setItem('cart', JSON.stringify(product));
+  // }
 
-  // Clear Cart 
-  clearCart(): void {
-    this.myStore.clear();
+  addToCart(product: Item[]): void {
+    let str = '[';
+    product.map((item, index) => {
+      str += `{
+        "id": ${item.id},
+        "name": "${item.name}",
+        "price": ${item.price},
+        "url": "${item.url}",
+        "description": "${item.description}",
+        "quantity": "${item.quantity}"
+      }`;
+      if (index !== product.length - 1) {
+        str += ',';
+      }
+    });
+    str += ']';
+    window.localStorage.setItem('cart', str);
   }
-
+  
+   // Clear Cart 
+  clearCartItems(): void {
+    window.localStorage.clear();
+  }
+  
   // Getting Items in a Cart
-  getCart(): CartProduct[] {
-    const getItem = this.myStore.getItem('cart');
-    return getItem? JSON.parse(getItem): [];
+  getItemsInCart(): Item[] {
+    const getItem = window.localStorage.getItem('cart');
+
+    if(!getItem){
+      return [];
+    }
+    return JSON.parse(getItem);
   }
 
 }
