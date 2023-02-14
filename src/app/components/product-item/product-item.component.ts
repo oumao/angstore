100732c8242de05865e2572c5af5237b396d88e3
oumao <evansotis2015@gmail.com>
2023-02-14 22:06:29 +0300

    Added emitter to pass data to parent Product-List

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { Product, productcount } from 'src/app/models/Product';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product, Item, productcount } from 'src/app/models/Product';
 import { CartService } from 'src/app/services/cart.service'
-import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-product-item',
@@ -11,8 +10,11 @@ import { HttpService } from 'src/app/services/http.service';
 export class ProductItemComponent {
   @Input() product: Product
 
-  pickedItem = '1'
+  @Output() addProductToCart = new EventEmitter<Product>();
+
+  
   productCount: string[] = productcount
+  pickedItem = '1'
 
   constructor(private cartService: CartService) {
     this.product = { id:0, name:'', price:0, url:'', description:''}
@@ -23,30 +25,6 @@ export class ProductItemComponent {
   }
 
   addItemToCart(item: Product): void{
-    const itemsInCart = this.cartService.getItemsInCart();
-    let itemInCart = itemsInCart.find((elem: any) => elem.id === item.id)
-
-    // Check to see if the item is already added
-    if(itemInCart){
-      itemInCart.quantity = this.pickedItem;
-    }else{
-      itemInCart = {
-        id: item.id,
-        name: item.name,
-        quantity: this.pickedItem,
-        price: item.price,
-        url: item.url,
-        description: item.description
-      }
-      itemsInCart.push(itemInCart);
-    }
-
-    // Store the updated items in the cart
-    this.cartService.addToCart(itemsInCart);
-    // alert(`${itemInCart.quantity} of ${itemInCart.name} added to cart`);
-    alert(`${itemInCart.quantity} ${itemInCart.name}${Number(itemInCart.quantity) > 1 ? 's' : ''} added to cart`);
-
-
-
+    this.addProductToCart.emit(item);
   }
 }
