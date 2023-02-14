31a2032e14c8a5249bc85f6cd334f7b173828589
oumao <evansotis2015@gmail.com>
2023-02-14 22:05:00 +0300

    Refactor code

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'
 
 import { Product } from '../models/Product';
-import { Item } from '../models/Product';
+
 
 
 @Injectable({
